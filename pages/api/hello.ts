f43ba0133831c8e9cab5443ac3ac8ab56d55a009
joven <wangflood@gmail.com>
2024-02-25 11:57:22 +0800

    mock

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { mock,Random } from 'mockjs'
 
 type Data = {
-  name: string;
-};
+  name: string
+}
 
-export default function handler(
+export default function handler (
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(
+    mock({
+      'list|1-10': [
+        {
+          'id|+1': 1,
+          name: Random.cname(),
+          date: Random.date(),
+          email: Random.email(),
+        }
+      ]
+    })
+  )
 }
